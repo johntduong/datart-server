d738a2385b042a968002394165201f740aca03e1
johntduong <johntduong@yahoo.com>
2018-02-18 22:10:02 -0800

    changed procfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
+const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 
 app.use(bodyParser.urlencoded({ extended: true }));
